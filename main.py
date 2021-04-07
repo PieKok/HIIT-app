79de0d34d1a613551b06e497385ad4208c69d56b
PieKok <pierre.kokou@hotmail.com>
2021-04-07 21:27:56 +0200

    Add images as icon for the list of exercises in function of the exercise category

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivymd.uix.list import OneLineListItem
+from kivymd.uix.list import OneLineAvatarListItem, ImageLeftWidget
 import sqlite3
 
+
+def attribute_image_file(category):
+    images_files_switcher = {
+        "Arms": "images/icon-arm.jpg",
+        "Cardio": "images/icon-cardio.png",
+        "Core": "images/icons-core.jpg",
+        "Legs": "images/icons-legs.jpg"
+    }
+    return images_files_switcher.get(category, "images/question_mark.jpg")
+
+
 class HIITApp(MDApp):
-    connection = None;
-    cursor = None;
+    connection = None
+    cursor = None
 
     def on_start(self):
         # Load the exercise database
@@ -24,15 +35,21 @@ class MainScreen(Screen):
 
 class ExerciseScreen(Screen):
     def on_pre_enter(self, *args):
-        self.ids.containerList.clear_widgets() # Clear the list if it had been loaded in the past
-
         # Connect to the database and load the exercises
         app = MDApp.get_running_app()
         sql_statement = "SELECT * FROM exercises"
         app.cursor.execute(sql_statement)
         exercises = app.cursor.fetchall()
+        self.display_list_of_exercises(exercises)
+
+    def display_list_of_exercises(self, exercises):
+        # Clear the list if it had been loaded in the past
+        self.ids.containerList.clear_widgets()
         for ex in exercises:
-            items = OneLineListItem(text=ex[0])
+            items = OneLineAvatarListItem(text=ex[0])
+            image_path = attribute_image_file(ex[1])
+            image_widget = ImageLeftWidget(source=image_path)
+            items.add_widget(image_widget)
             self.ids.containerList.add_widget(items)
 
 
