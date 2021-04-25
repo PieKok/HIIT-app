6a6a339781590f5b62ac3378d8d9c720e574e904
PieKok <pierre.kokou@hotmail.com>
2021-04-25 22:32:11 +0200

    Finish "add exercise" dialog and its logic, implement "open exercise" dialog and logic (incl deleting the exercise)

--- exerciseScreen.py
+++ exerciseScreen.py
@@ -4,7 +4,6 @@ from kivymd.app import MDApp
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton
-from kivymd.uix.textfield import MDTextField
 from kivymd.uix.menu import MDDropdownMenu
 
 
@@ -20,6 +19,7 @@ def attribute_image_file(category):
 
 class ExerciseScreen(Screen):
     add_ex_dialog = None
+    ex_dialog = None
 
     def on_pre_enter(self, *args):
         self.display_all_exercises()
@@ -36,7 +36,7 @@ class ExerciseScreen(Screen):
         # Clear the list if it had been loaded in the past
         self.ids.containerList.clear_widgets()
         for ex in exercises:
-            items = OneLineAvatarListItem(text=ex[0])
+            items = OneLineAvatarListItem(text=ex[0], on_release=self.open_exercise)
             image_path = attribute_image_file(ex[1])
             image_widget = ImageLeftWidget(source=image_path)
             items.add_widget(image_widget)
@@ -65,29 +65,94 @@ class ExerciseScreen(Screen):
                     )
                 ]
             )
-        # self.add_ex_dialog.set_normal_height()
         self.add_ex_dialog.open()
 
     def cancel_add(self, inst):
         self.add_ex_dialog.dismiss()
-        print('Cancel')
 
     def add_ex(self, inst):
-        my_name = self.add_ex_dialog.content_cls.ids.field_name.text
+        my_name = self.add_ex_dialog.content_cls.ids.field_name.text.capitalize()
         my_category = self.add_ex_dialog.content_cls.ids.dropdown_item.current_item
         if my_category != '' and my_name != '' and self.check_name_ex(my_name):
-            print(my_name)
-            print(my_category)
+            str_equipment = ""
+            if self.add_ex_dialog.content_cls.ids.check_jumping.active:
+                str_equipment = str_equipment + "jumping / "
+            if self.add_ex_dialog.content_cls.ids.check_running.active:
+                str_equipment = str_equipment + "running / "
+            if self.add_ex_dialog.content_cls.ids.check_mattress.active:
+                str_equipment = str_equipment + "mattress / "
+            if self.add_ex_dialog.content_cls.ids.check_skip.active:
+                str_equipment = str_equipment + "skipping rope"
+
+            app = MDApp.get_running_app()
+            app.cursor.execute("INSERT INTO exercises (name, category, equipment) VALUES (?, ?, ?);",
+                               [my_name, my_category, str_equipment])
+            app.connection.commit()
+
+            self.display_all_exercises()
             self.add_ex_dialog.dismiss()
         else:
             pass
 
     def check_name_ex(self, my_name):
         app = MDApp.get_running_app()
-        sql_statement = "SELECT * FROM exercises WHERE name='" + my_name.capitalize() + "'"
+        sql_statement = "SELECT * FROM exercises WHERE name='" + my_name + "'"
         app.cursor.execute(sql_statement)
         is_present = app.cursor.fetchall()
         return not is_present
+
+    def open_exercise(self, inst):
+        print(inst.text)
+
+        app = MDApp.get_running_app()
+        sql_statement = "SELECT * FROM exercises WHERE name='" + inst.text + "'"
+        app.cursor.execute(sql_statement)
+        my_exercise = app.cursor.fetchall()
+
+        self.ex_dialog = MDDialog(
+            type="custom",
+            content_cls=ExerciseDialogContent(ex=my_exercise),
+            size_hint=(0.8, 0.8),
+            buttons=[
+                MDFlatButton(
+                    text="Cancel", on_release=self.cancel_ex_dialog
+                ),
+                MDFlatButton(
+                    text="Delete", on_release=self.delete_ex
+                )
+            ]
+        )
+        self.ex_dialog.open()
+
+    def cancel_ex_dialog(self, inst):
+        self.ex_dialog.dismiss()
+
+    def delete_ex(self, inst):
+        my_ex_name = self.ex_dialog.content_cls.ids.field_name.text
+        app = MDApp.get_running_app()
+        app.cursor.execute("DELETE FROM exercises WHERE name='" + my_ex_name + "'")
+        app.connection.commit()
+
+        self.display_all_exercises()
+        self.ex_dialog.dismiss()
+
+
+class ExerciseDialogContent(BoxLayout):
+    def __init__(self, ex, **kwargs):
+        super().__init__(**kwargs)
+
+        self.ids.field_name.text = ex[0][0]
+        self.ids.field_category.text = ex[0][1]
+
+        if "jumping" in ex[0][2]:
+            self.ids.check_jumping.active = True
+        if "running" in ex[0][2]:
+            self.ids.check_running.active = True
+        if "mattress" in ex[0][2]:
+            self.ids.check_mattress.active = True
+        if "skipping rope" in ex[0][2]:
+            self.ids.check_skip.active = True
+
 
 class AddExerciseDialogContent(BoxLayout):
     def __init__(self, **kwargs):
